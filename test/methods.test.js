d6cecaf68165c0228a042146a98a2dd19674ff60
delvedor <tommydelved@gmail.com>
2017-04-17 10:43:34 +0200

    Added test

---
+++ test/methods.test.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const FindMyWay = require('../')
+
+test('the router is an object with methods', t => {
+  t.plan(2)
+
+  const findMyWay = FindMyWay()
+
+  t.is(typeof findMyWay.on, 'function')
+  t.is(typeof findMyWay.lookup, 'function')
+})
+
+test('register a routre', t => {
+  t.plan(1)
+  const findMyWay = FindMyWay()
+
+  findMyWay.on('GET', '/test', () => {
+    t.ok('inside the handler')
+  })
+
+  findMyWay.lookup('GET', '/test')
+})
+
+test('default route', t => {
+  t.plan(1)
+
+  const findMyWay = FindMyWay({
+    defaultRoute: () => {
+      t.ok('inside the default route')
+    }
+  })
+
+  findMyWay.lookup('GET', '/test')
+})
+
+test('async handler', t => {
+  t.plan(1)
+
+  const findMyWay = FindMyWay({
+    async: true
+  })
+
+  findMyWay.on('GET', '/test', (req, res, params) => {
+    t.ok('inside async handler')
+  })
+
+  findMyWay.lookup('GET', '/test')
+})
+
+test('parametric route', t => {
+  t.plan(1)
+  const findMyWay = FindMyWay()
+
+  findMyWay.on('GET', '/test/:id', (req, res, params) => {
+    t.is(params.id, 'hello')
+  })
+
+  findMyWay.lookup('GET', '/test/hello')
+})
+
+test('multiple parametric route', t => {
+  t.plan(2)
+  const findMyWay = FindMyWay()
+
+  findMyWay.on('GET', '/test/:id', (req, res, params) => {
+    t.is(params.id, 'hello')
+  })
+
+  findMyWay.on('GET', '/other-test/:id', (req, res, params) => {
+    t.is(params.id, 'world')
+  })
+
+  findMyWay.lookup('GET', '/test/hello')
+  findMyWay.lookup('GET', '/other-test/world')
+})
+
+test('multiple parametric route with the same prefix', t => {
+  t.plan(2)
+  const findMyWay = FindMyWay()
+
+  findMyWay.on('GET', '/test/:id', (req, res, params) => {
+    t.is(params.id, 'hello')
+  })
+
+  findMyWay.on('GET', '/test/:id/world', (req, res, params) => {
+    t.is(params.id, 'world')
+  })
+
+  findMyWay.lookup('GET', '/test/hello')
+  findMyWay.lookup('GET', '/test/world/world')
+})
+
+test('nested parametric route', t => {
+  t.plan(2)
+  const findMyWay = FindMyWay()
+
+  findMyWay.on('GET', '/test/:hello/test/:world', (req, res, params) => {
+    t.is(params.hello, 'hello')
+    t.is(params.world, 'world')
+  })
+
+  findMyWay.lookup('GET', '/test/hello/test/world')
+})
+
+test('wildcard', t => {
+  t.plan(1)
+  const findMyWay = FindMyWay()
+
+  findMyWay.on('GET', '/test/*', (req, res, params) => {
+    t.is(params['*'], 'hello')
+  })
+
+  findMyWay.lookup('GET', '/test/hello')
+})
---
+++ test/server.test.js
@@ -0,0 +1,92 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const http = require('http')
+const request = require('request')
+const FindMyWay = require('../')
+
+test('basic router with http server', t => {
+  t.plan(7)
+  const findMyWay = FindMyWay()
+  findMyWay.on('GET', '/test', (req, res, params) => {
+    t.ok(req)
+    t.ok(res)
+    t.ok(params)
+    res.end(JSON.stringify({ hello: 'world' }))
+  })
+
+  const server = http.createServer((req, res) => {
+    findMyWay.lookup(req.method, req.url, req, res)
+  })
+
+  server.listen(0, err => {
+    t.error(err)
+    server.unref()
+
+    request({
+      method: 'GET',
+      uri: 'http://localhost:' + server.address().port + '/test'
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.deepEqual(JSON.parse(body), { hello: 'world' })
+    })
+  })
+})
+
+test('router with params with http server', t => {
+  t.plan(7)
+  const findMyWay = FindMyWay()
+  findMyWay.on('GET', '/test/:id', (req, res, params) => {
+    t.ok(req)
+    t.ok(res)
+    t.is(params.id, 'hello')
+    res.end(JSON.stringify({ hello: 'world' }))
+  })
+
+  const server = http.createServer((req, res) => {
+    findMyWay.lookup(req.method, req.url, req, res)
+  })
+
+  server.listen(0, err => {
+    t.error(err)
+    server.unref()
+
+    request({
+      method: 'GET',
+      uri: 'http://localhost:' + server.address().port + '/test/hello'
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.deepEqual(JSON.parse(body), { hello: 'world' })
+    })
+  })
+})
+
+test('default route', t => {
+  t.plan(3)
+  const findMyWay = FindMyWay({
+    defaultRoute: (req, res) => {
+      res.statusCode = 404
+      res.end()
+    }
+  })
+
+  const server = http.createServer((req, res) => {
+    findMyWay.lookup(req.method, req.url, req, res)
+  })
+
+  server.listen(0, err => {
+    t.error(err)
+    server.unref()
+
+    request({
+      method: 'GET',
+      uri: 'http://localhost:' + server.address().port
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 404)
+    })
+  })
+})
