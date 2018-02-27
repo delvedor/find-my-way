f09eef0141df4cd7e1a284bcbc30b25738c6a0d7
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2018-02-27 17:04:40 +0100

    Update issue-63 test case

--- test/issue-63.test.js
+++ test/issue-63.test.js
@@ -5,7 +5,10 @@ const factory = require('../')
 
 t.test('issue-63', (t) => {
   t.plan(1)
+
   const fmw = factory()
 
-  fmw.on('GET', '/foo/:id(a', function () {})
+  t.throws(function () {
+    fmw.on('GET', '/foo/:id(a', function () {})
+  })
 })
