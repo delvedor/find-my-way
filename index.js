71d7fcfc4e6ab1a27d1e8f3b1e84a1e4774da856
James Sumners <james.sumners@gmail.com>
2018-04-18 09:00:25 -0400

    Add comment explaining ; split

--- index.js
+++ index.js
@@ -444,6 +444,9 @@ module.exports = Router
 function sanitizeUrl (url) {
   for (var i = 0, len = url.length; i < len; i++) {
     var charCode = url.charCodeAt(i)
+    // Some systems do not follow RFC and separate the path and query
+    // string with a `;` character (code 59), e.g. `/foo;jsessionid=123456`.
+    // Thus, we need to split on `;` as well as `?` and `#`.
     if (charCode === 63 || charCode === 59 || charCode === 35) {
       return url.slice(0, i)
     }
