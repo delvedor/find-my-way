5e3f16bc8ff159c2c665d0f7e9098dc6b0528ac3
delvedor <tommydelved@gmail.com>
2018-05-20 21:46:01 +0200

    Updated test

--- test/issue-62.test.js
+++ test/issue-62.test.js
@@ -1,28 +1,28 @@
 'use strict'
 
 const t = require('tap')
-const factory = require('../')
+const FindMyWay = require('../')
 
 const noop = function () {}
 
 t.test('issue-62', (t) => {
   t.plan(2)
 
-  const fmw = factory()
+  const findMyWay = FindMyWay({ allowUnsafeRegex: true })
 
-  fmw.on('GET', '/foo/:id(([a-f0-9]{3},?)+)', noop)
+  findMyWay.on('GET', '/foo/:id(([a-f0-9]{3},?)+)', noop)
 
-  t.notOk(fmw.find('GET', '/foo/qwerty'))
-  t.ok(fmw.find('GET', '/foo/bac,1ea'))
+  t.notOk(findMyWay.find('GET', '/foo/qwerty'))
+  t.ok(findMyWay.find('GET', '/foo/bac,1ea'))
 })
 
 t.test('issue-62 - escape chars', (t) => {
-  const fmw = factory()
+  const findMyWay = FindMyWay()
 
   t.plan(2)
 
-  fmw.get('/foo/:param(\\([a-f0-9]{3}\\))', noop)
+  findMyWay.get('/foo/:param(\\([a-f0-9]{3}\\))', noop)
 
-  t.notOk(fmw.find('GET', '/foo/abc'))
-  t.ok(fmw.find('GET', '/foo/(abc)'))
+  t.notOk(findMyWay.find('GET', '/foo/abc'))
+  t.ok(findMyWay.find('GET', '/foo/(abc)'))
 })
