cd4206e93651685ad39d6c35efb604c710dcff3b
PoppinL <poppinlp@gmail.com>
2018-09-15 11:16:20 +0800

    test: update test cases for issue 93

--- test/issue-93.test.js
+++ test/issue-93.test.js
@@ -6,12 +6,15 @@ const FindMyWay = require('../')
 const noop = () => {}
 
 test('Should keep semver store when split node', t => {
-  t.plan(1)
+  t.plan(4)
 
   const findMyWay = FindMyWay()
 
-  findMyWay.on('GET', '/t1', { version: '0.1.0' }, noop)
-  findMyWay.on('GET', '/t2', { version: '0.1.0' }, noop)
+  findMyWay.on('GET', '/t1', { version: '1.0.0' }, noop)
+  findMyWay.on('GET', '/t2', { version: '2.1.0' }, noop)
 
-  t.ok(findMyWay.find('GET', '/t1', '0.1.0'))
+  t.ok(findMyWay.find('GET', '/t1', '1.0.0'))
+  t.ok(findMyWay.find('GET', '/t2', '2.x'))
+  t.notOk(findMyWay.find('GET', '/t1', '2.x'))
+  t.notOk(findMyWay.find('GET', '/t2', '1.0.0'))
 })
