c175f1d319e9ce2a8c7084f1ad149482e09b18ba
Ipalibo Whyte <ipalibo@august.com>
2018-12-14 09:36:39 -0800

    - added test from issue

--- test/issue-104.test.js
+++ test/issue-104.test.js
@@ -2,6 +2,33 @@ const t = require('tap')
 const test = t.test
 const FindMyWay = require('../')
 
+test('Nested static parametric route, url with parameter common prefix > 1', t => {
+  t.plan(1)
+  const findMyWay = FindMyWay({
+    defaultRoute: (req, res) => {
+      t.fail('Should not be defaultRoute')
+    }
+  })
+
+  findMyWay.on('GET', '/a/bbbb', (req, res) => {
+    res.end('{"message":"hello world"}')
+  })
+
+  findMyWay.on('GET', '/a/bbaa', (req, res) => {
+    res.end('{"message":"hello world"}')
+  })
+
+  findMyWay.on('GET', '/a/babb', (req, res) => {
+    res.end('{"message":"hello world"}')
+  })
+
+  findMyWay.on('DELETE', '/a/:id', (req, res) => {
+    res.end('{"message":"hello world"}')
+  })
+
+  t.deepEqual(findMyWay.find('DELETE', '/a/bbar').params, { 'id': 'bbar' })
+})
+
 test('Parametric route, url with parameter common prefix > 1', t => {
   t.plan(1)
   const findMyWay = FindMyWay({
