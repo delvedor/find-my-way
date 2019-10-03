1e69308b9d7dbeb8b8f20dd6690b4090362810c1
delvedor <tommydelved@gmail.com>
2019-10-03 17:23:04 +0200

    Updated test

--- test/issue-132.test.js
+++ test/issue-132.test.js
@@ -4,7 +4,7 @@ const t = require('tap')
 const test = t.test
 const FindMyWay = require('../')
 
-test('Wildcard mixed with dynamic and common prefix', t => {
+test('Wildcard mixed with dynamic and common prefix / 1', t => {
   t.plan(5)
   const findMyWay = FindMyWay({
     defaultRoute: (req, res) => {
@@ -27,6 +27,47 @@ test('Wildcard mixed with dynamic and common prefix', t => {
   findMyWay.on('GET', '/obj_params/*', (req, res, params) => {
     t.strictEqual(req.method, 'GET')
   })
+
+  findMyWay.lookup({ method: 'OPTIONS', url: '/obj/params', headers: {} }, null)
+
+  findMyWay.lookup({ method: 'OPTIONS', url: '/obj/params/12', headers: {} }, null)
+
+  findMyWay.lookup({ method: 'GET', url: '/obj/params/12', headers: {} }, null)
+
+  findMyWay.lookup({ method: 'OPTIONS', url: '/obj_params/12', headers: {} }, null)
+
+  findMyWay.lookup({ method: 'GET', url: '/obj_params/12', headers: {} }, null)
+})
+
+test('Wildcard mixed with dynamic and common prefix / 2', t => {
+  t.plan(6)
+  const findMyWay = FindMyWay({
+    defaultRoute: (req, res) => {
+      t.fail('Should not be defaultRoute')
+    }
+  })
+
+  findMyWay.on('OPTIONS', '/*', (req, res, params) => {
+    t.strictEqual(req.method, 'OPTIONS')
+  })
+
+  findMyWay.on('OPTIONS', '/obj/*', (req, res, params) => {
+    t.strictEqual(req.method, 'OPTIONS')
+  })
+
+  findMyWay.on('GET', '/obj/params/*', (req, res, params) => {
+    t.strictEqual(req.method, 'GET')
+  })
+
+  findMyWay.on('GET', '/obj/:id', (req, res, params) => {
+    t.strictEqual(req.method, 'GET')
+  })
+
+  findMyWay.on('GET', '/obj_params/*', (req, res, params) => {
+    t.strictEqual(req.method, 'GET')
+  })
+
+  findMyWay.lookup({ method: 'OPTIONS', url: '/obj_params/params', headers: {} }, null)
 
   findMyWay.lookup({ method: 'OPTIONS', url: '/obj/params', headers: {} }, null)
 
