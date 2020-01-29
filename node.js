06e7b29e6d702b64c40390188e850446b0bab436
Elian Cordoba <eliancordoba98@gmail.com>
2020-01-29 16:44:55 -0300

    Log handler names and deleted old method

--- node.js
+++ node.js
@@ -190,15 +190,16 @@ Node.prototype.getVersionHandler = function (version, method) {
   return handlers === null ? handlers : handlers[method]
 }
 
-Node.prototype.prettyPrintNew = function () {
+Node.prototype.prettyPrint = function () {
   const tree = { ...this }
   traverseTree(tree)
 
   // Moneky patch needed for the routes that have multiple handlers
   return archy(tree)
     .replace(/─┬/g, '')
-    .replace(/│ ├── &/g, '├── ')
-    .replace(/│ └── &/g, '├── ')
+    .replace(/│ ├── & /g, '├── ')
+    .replace(/│ └── & /g, '├── ')
+    .replace(/ & /g, ' ')
 }
 
 function traverseTree (node) {
@@ -223,9 +224,9 @@ function traverseTree (node) {
         let label
         // The & is to later find this branches and fix them
         if (node.prefix === ':') {
-          label = `&:${route[1].params.join()} (${route[0]}) ${route[0][1].handler.name}`
+          label = `& :${route[1].params.join()} (${route[0]}) ${route[1].handler.name}`
         } else {
-          label = `&(${route[0]}) ${route[0][1].handler.name}`
+          label = `& (${route[0]}) ${route[1].handler.name}`
         }
 
         return { label: label }
@@ -244,43 +245,6 @@ function traverseTree (node) {
 
 function mapChildredToArray (children) {
   return Object.keys(children).map(i => children[i])
-}
-
-Node.prototype.prettyPrint = function (prefix, tail) {
-  var paramName = ''
-  var handlers = this.handlers || {}
-  var methods = Object.keys(handlers).filter(method => handlers[method] && handlers[method].handler)
-
-  if (this.prefix === ':') {
-    methods.forEach((method, index) => {
-      var params = this.handlers[method].params
-      var param = params[params.length - 1]
-      if (methods.length > 1) {
-        if (index === 0) {
-          paramName += param + ` (${method})\n`
-          return
-        }
-        paramName += prefix + '    :' + param + ` (${method})`
-        paramName += (index === methods.length - 1 ? '' : '\n')
-      } else {
-        paramName = params[params.length - 1] + ` (${method})`
-      }
-    })
-  } else if (methods.length) {
-    paramName = ` (${methods.join('|')})`
-  }
-
-  var tree = `${prefix}${tail ? '└── ' : '├── '}${this.prefix}${paramName}\n`
-
-  prefix = `${prefix}${tail ? '    ' : '│   '}`
-  const labels = Object.keys(this.children)
-  for (var i = 0; i < labels.length - 1; i++) {
-    tree += this.children[labels[i]].prettyPrint(prefix, false)
-  }
-  if (labels.length > 0) {
-    tree += this.children[labels[labels.length - 1]].prettyPrint(prefix, true)
-  }
-  return tree
 }
 
 function buildHandlers (handlers) {
