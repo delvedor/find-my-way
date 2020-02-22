c4db3bb3b9119cc33c3fa1b94838bb2f25293bb2
Manuel Spigolon <behemoth89@gmail.com>
2020-02-22 22:17:45 +0100

    more test check

--- test/issue-145.test.js
+++ test/issue-145.test.js
@@ -4,7 +4,7 @@ const t = require('tap')
 const FindMyWay = require('../')
 
 t.test('issue-145', (t) => {
-  t.plan(4)
+  t.plan(8)
 
   const findMyWay = FindMyWay({ ignoreTrailingSlash: true })
 
@@ -14,7 +14,11 @@ t.test('issue-145', (t) => {
   findMyWay.on('GET', '/a/:pam/c', varPath)
 
   t.equals(findMyWay.find('GET', '/a/b').handler, fixedPath)
+  t.equals(findMyWay.find('GET', '/a/b/').handler, fixedPath)
   t.equals(findMyWay.find('GET', '/a/b/c').handler, varPath)
+  t.equals(findMyWay.find('GET', '/a/b/c/').handler, varPath)
   t.equals(findMyWay.find('GET', '/a/foo/c').handler, varPath)
+  t.equals(findMyWay.find('GET', '/a/foo/c/').handler, varPath)
   t.notOk(findMyWay.find('GET', '/a/c'))
+  t.notOk(findMyWay.find('GET', '/a/c/'))
 })
