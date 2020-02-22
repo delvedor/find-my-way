21bde7e4701070f8afa1a03e0d34ed5c62776a9e
Manuel Spigolon <behemoth89@gmail.com>
2020-02-22 17:38:20 +0100

    fix more strict test

--- test/issue-145.test.js
+++ test/issue-145.test.js
@@ -3,20 +3,18 @@
 const t = require('tap')
 const FindMyWay = require('../')
 
-const noop = function () {}
-
 t.test('issue-145', (t) => {
   t.plan(4)
 
   const findMyWay = FindMyWay({ ignoreTrailingSlash: true })
 
-  findMyWay.on('GET', '/a/b', noop)
-  findMyWay.on('GET', '/a/:pam/c', noop)
+  const fixedPath = function staticPath () {}
+  const varPath = function parameterPath () {}
+  findMyWay.on('GET', '/a/b', fixedPath)
+  findMyWay.on('GET', '/a/:pam/c', varPath)
 
-  console.log(findMyWay.prettyPrint())
-
-  t.ok(findMyWay.find('GET', '/a/b'))
-  t.ok(findMyWay.find('GET', '/a/b/c'))
-  t.ok(findMyWay.find('GET', '/a/foo/c'))
+  t.equals(findMyWay.find('GET', '/a/b').handler, fixedPath)
+  t.equals(findMyWay.find('GET', '/a/b/c').handler, varPath)
+  t.equals(findMyWay.find('GET', '/a/foo/c').handler, varPath)
   t.notOk(findMyWay.find('GET', '/a/c'))
 })
