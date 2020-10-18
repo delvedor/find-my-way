df674d0c191be0dd84bd26455a20173f29759f4e
Ayoub El Khattabi <elkhattabi.ayoub@gmail.com>
2020-10-18 17:08:19 +0100

    update Node.getMatchingHandler to add hasConstraint boolean

--- node.js
+++ node.js
@@ -184,15 +184,23 @@ Node.prototype.getConstraintsHandler = function (constraints, method) {
 }
 
 Node.prototype.getMatchingHandler = function (derivedConstraints, method) {
-  if (derivedConstraints && this.kConstraints.length) {
-    var constraints, handler
-    for (var i = 0; i < this.kConstraints.length; i++) {
-      constraints = {}
-      for (var j = 0; j < this.kConstraints[i].length; j++) {
-        constraints[this.kConstraints[i][j]] = derivedConstraints[this.kConstraints[i][j]]
+  if (derivedConstraints) {
+    if (this.kConstraints.length) {
+      var constraints, handler, hasConstraint
+      for (var i = 0; i < this.kConstraints.length; i++) {
+        hasConstraint = false
+        constraints = {}
+        for (var j = 0; j < this.kConstraints[i].length; j++) {
+          if (derivedConstraints[this.kConstraints[i][j]]) {
+            hasConstraint = true
+            constraints[this.kConstraints[i][j]] = derivedConstraints[this.kConstraints[i][j]]
+          }
+        }
+        if (hasConstraint) {
+          handler = this.getConstraintsHandler(constraints, method)
+          if (handler) return handler
+        }
       }
-      handler = this.getConstraintsHandler(constraints, method)
-      if (handler) return handler
     }
   }
 
