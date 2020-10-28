ef1e8ca88ed6448b1fc957dfd4557e44edf82827
Harry Brundage <harry.brundage@gmail.com>
2020-10-28 11:25:58 -0400

    Add a test and fix host regex constraining

--- test/constraint.host.test.js
+++ test/constraint.host.test.js
@@ -7,7 +7,7 @@ const alpha = () => { }
 const beta = () => { }
 const gamma = () => { }
 
-test('A route could support multiple host constraints', t => {
+test('A route supports multiple host constraints', t => {
   t.plan(4)
 
   const findMyWay = FindMyWay()
@@ -20,6 +20,20 @@ test('A route could support multiple host constraints', t => {
   t.strictEqual(findMyWay.find('GET', '/', { host: 'something-else.io' }).handler, alpha)
   t.strictEqual(findMyWay.find('GET', '/', { host: 'fastify.io' }).handler, beta)
   t.strictEqual(findMyWay.find('GET', '/', { host: 'example.com' }).handler, gamma)
+})
+
+test('A route supports wildcard host constraints', t => {
+  t.plan(4)
+
+  const findMyWay = FindMyWay()
+
+  findMyWay.on('GET', '/', { constraints: { host: 'fastify.io' } }, beta)
+  findMyWay.on('GET', '/', { constraints: { host: /.*\.fastify\.io/ } }, gamma)
+
+  t.strictEqual(findMyWay.find('GET', '/', { host: 'fastify.io' }).handler, beta)
+  t.strictEqual(findMyWay.find('GET', '/', { host: 'foo.fastify.io' }).handler, gamma)
+  t.strictEqual(findMyWay.find('GET', '/', { host: 'bar.fastify.io' }).handler, gamma)
+  t.notOk(findMyWay.find('GET', '/', { host: 'example.com' }))
 })
 
 test('A route could support multiple host constraints while versioned', t => {
