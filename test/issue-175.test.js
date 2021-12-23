e30cafd1c2495944e8861e4f1338ab9ce83ccf16
Ivan Timoshenko <bugagashenkj@gmail.com>
2021-12-23 17:39:01 +0200

    fix: double colon route issue (#219) (#220)

--- test/issue-175.test.js
+++ test/issue-175.test.js
@@ -29,3 +29,53 @@ test('exactly one match for static route with colon', t => {
   t.equal(findMyWay.find('GET', '/name:customVerb').handler, handler)
   t.equal(findMyWay.find('GET', '/name:test'), null)
 })
+
+test('double colon is replaced with single colon, no parameters, same parent node name', t => {
+  t.plan(1)
+  const findMyWay = FindMyWay({
+    defaultRoute: () => t.fail('should not be default route')
+  })
+
+  findMyWay.on('GET', '/name', () => {
+    t.fail('should not be parent route')
+  })
+
+  findMyWay.on('GET', '/name::customVerb', (req, res, params) => {
+    t.same(params, {})
+  })
+
+  findMyWay.lookup({ method: 'GET', url: '/name:customVerb', headers: {} }, null)
+})
+
+test('double colon is replaced with single colon, default route, same parent node name', t => {
+  t.plan(1)
+  const findMyWay = FindMyWay({
+    defaultRoute: () => t.ok('should be default route')
+  })
+
+  findMyWay.on('GET', '/name', () => {
+    t.fail('should not be parent route')
+  })
+
+  findMyWay.on('GET', '/name::customVerb', () => {
+    t.fail('should not be child route')
+  })
+
+  findMyWay.lookup({ method: 'GET', url: '/name:wrongCustomVerb', headers: {} }, null)
+})
+
+test('double colon is replaced with single colon, with parameters', t => {
+  t.plan(1)
+  const findMyWay = FindMyWay({
+    defaultRoute: () => t.fail('should not be default route')
+  })
+
+  findMyWay.on('GET', '/name1::customVerb1/:param1/name2::customVerb2:param2', (req, res, params) => {
+    t.same(params, {
+      param1: 'value1',
+      param2: 'value2'
+    })
+  })
+
+  findMyWay.lookup({ method: 'GET', url: '/name1:customVerb1/value1/name2:customVerb2value2', headers: {} }, null)
+})
