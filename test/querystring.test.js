349f8e4f474d67083e4008641ad44fa04d3c29a1
Ivan Tymoshenko <bugagashenkj@gmail.com>
2022-05-18 13:55:03 +0300

    Add querrystring parser (#284)

--- test/querystring.test.js
+++ test/querystring.test.js
@@ -5,10 +5,11 @@ const test = t.test
 const FindMyWay = require('../')
 
 test('should sanitize the url - query', t => {
-  t.plan(1)
+  t.plan(2)
   const findMyWay = FindMyWay()
 
-  findMyWay.on('GET', '/test', (req, res, params) => {
+  findMyWay.on('GET', '/test', (req, res, params, store, query) => {
+    t.same(query, { hello: 'world' })
     t.ok('inside the handler')
   })
 
@@ -16,10 +17,11 @@ test('should sanitize the url - query', t => {
 })
 
 test('should sanitize the url - hash', t => {
-  t.plan(1)
+  t.plan(2)
   const findMyWay = FindMyWay()
 
-  findMyWay.on('GET', '/test', (req, res, params) => {
+  findMyWay.on('GET', '/test', (req, res, params, store, query) => {
+    t.same(query, { hello: '' })
     t.ok('inside the handler')
   })
 
@@ -27,10 +29,11 @@ test('should sanitize the url - hash', t => {
 })
 
 test('handles path and query separated by ;', t => {
-  t.plan(1)
+  t.plan(2)
   const findMyWay = FindMyWay()
 
-  findMyWay.on('GET', '/test', (req, res, params) => {
+  findMyWay.on('GET', '/test', (req, res, params, store, query) => {
+    t.same(query, { jsessionid: '123456' })
     t.ok('inside the handler')
   })
 
