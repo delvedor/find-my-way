3929cca914a1826c04759fbb8e15fbe9cf9c7049
Voon Wong <voon.wong@smartbear.com>
2025-02-01 07:12:20 +1100

    chore: fix broken test

--- test/types/router.test-d.ts
+++ test/types/router.test-d.ts
@@ -17,7 +17,7 @@ let http2Res!: Http2ServerResponse;
     allowUnsafeRegex: false,
     caseSensitive: false,
     maxParamLength: 42,
-    querystringParser: (queryString) {},
+    querystringParser: (queryString) => {},
     defaultRoute (http1Req, http1Res) {},
     onBadUrl (path, http1Req, http1Res) {},
     constraints: {
@@ -93,7 +93,7 @@ let http2Res!: Http2ServerResponse;
     allowUnsafeRegex: false,
     caseSensitive: false,
     maxParamLength: 42,
-    querystringParser: (queryString) {},
+    querystringParser: (queryString) => {},
     defaultRoute (http1Req, http1Res) {},
     onBadUrl (path, http1Req, http1Res) {},
     constraints
