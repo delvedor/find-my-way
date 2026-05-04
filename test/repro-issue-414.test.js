ac7011536766ed4189ae5e670b91f1a18460367e
oldregime <oldregime@users.noreply.github.com>
2026-05-04 06:50:35 +0530

    chore: fix linting errors (trailing spaces)

--- test/repro-issue-414.test.js
+++ test/repro-issue-414.test.js
@@ -7,7 +7,7 @@ test('should return null when maxParamLength is exceeded (current behavior)', t 
   t.plan(1)
   const findMyWay = FindMyWay({ maxParamLength: 5 })
   findMyWay.on('GET', '/test/:param', () => 'param')
-  
+
   const handle = findMyWay.find('GET', '/test/123456')
   t.assert.equal(handle, null)
 })
@@ -17,7 +17,7 @@ test('should still match other routes if one parametric route exceeds maxParamLe
   const findMyWay = FindMyWay({ maxParamLength: 5 })
   findMyWay.on('GET', '/test/:param', () => 'param')
   findMyWay.on('GET', '/test/special', () => 'special')
-  
+
   const handle = findMyWay.find('GET', '/test/special')
   t.assert.ok(handle)
   t.assert.equal(handle.handler(), 'special')
@@ -27,7 +27,7 @@ test('should fail to match any route if the only candidate exceeds maxParamLengt
   t.plan(1)
   const findMyWay = FindMyWay({ maxParamLength: 5 })
   findMyWay.on('GET', '/test/:param', () => 'param')
-  
+
   const handle = findMyWay.find('GET', '/test/123456789')
   t.assert.equal(handle, null)
 })
@@ -37,7 +37,7 @@ test('should match wildcard if parametric exceeds maxParamLength', t => {
   const findMyWay = FindMyWay({ maxParamLength: 5 })
   findMyWay.on('GET', '/test/:param', () => 'param')
   findMyWay.on('GET', '/test/*', () => 'wildcard')
-  
+
   const handle = findMyWay.find('GET', '/test/123456789')
   t.assert.ok(handle)
   t.assert.equal(handle.handler(), 'wildcard')
@@ -45,12 +45,12 @@ test('should match wildcard if parametric exceeds maxParamLength', t => {
 
 test('should return custom onMaxParamLength handler if provided and no other route matches', t => {
   t.plan(2)
-  const findMyWay = FindMyWay({ 
+  const findMyWay = FindMyWay({
     maxParamLength: 5,
     onMaxParamLength: (path, req, res) => 'custom error'
   })
   findMyWay.on('GET', '/test/:param', () => 'param')
-  
+
   const handle = findMyWay.find('GET', '/test/123456')
   t.assert.ok(handle)
   t.assert.equal(handle.handler(), 'custom error')
